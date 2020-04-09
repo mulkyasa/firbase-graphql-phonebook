6ea58d2197e634c26f1675fd66163316021f9a84
mulkyasa <mulkyasa@gmail.com>
2020-04-09 09:14:27 +0700

    API created

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,4 +16,52 @@ router.get('/', function(req, res) {
   });
 });
 
+//Create new instance
+router.post('/', function (req, res) {
+  const userName = req.body.username;
+  const name = req.body.name;
+  const number = req.body.number;
+
+  const referencePath = '/Phonebook/'+userName+'/';
+  const phonebookReference = firebase.database().ref(referencePath);
+  phonebookReference.set({Name: name, Number: number}, function(error) {
+    if (error) {
+      res.send("Data could not be saved." + error);
+    } else {
+      res.send("Data saved successfully.");
+    }
+  });
+});
+
+//Update existing instance
+router.put('/:username', function (req, res) {
+  const userName = req.params.username;
+  const name = req.body.name;
+  const number = req.body.number;
+
+  const referencePath = '/Phonebook/'+userName+'/';
+  const phonebookReference = firebase.database().ref(referencePath);
+  phonebookReference.update({Name: name, Number: number}, function(error) {
+    if (error) {
+      res.send("Data could not be updated." + error);
+    } else {
+      res.send("Data updated successfully.");
+    }
+  });
+});
+
+//Delete an instance
+router.delete('/:username', function (req, res) {
+  var userName = req.params.username;
+  var referencePath = '/Phonebook/'+userName+'/';
+  var userReference = firebase.database().ref(referencePath);
+  userReference.remove((error)=>{
+    if (error) {
+      res.send("Data could not be deleted." + error);
+    } else {
+      res.send("Data deleted successfully.");
+    }
+  })
+});
+
 module.exports = router;
